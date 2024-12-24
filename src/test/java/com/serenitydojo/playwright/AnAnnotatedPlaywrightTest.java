8aaa1bbcedf98a29dd2341778c4f45a698febacf
flaviaadiniz <flaviaadiniz@gmail.com>
2024-12-24 11:48:33 -0300

    Add code to set broswer launch options

--- src/test/java/com/serenitydojo/playwright/AnAnnotatedPlaywrightTest.java
+++ src/test/java/com/serenitydojo/playwright/AnAnnotatedPlaywrightTest.java
@@ -1,12 +1,31 @@
 package com.serenitydojo.playwright;
 
+import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Page;
+import com.microsoft.playwright.junit.Options;
+import com.microsoft.playwright.junit.OptionsFactory;
 import com.microsoft.playwright.junit.UsePlaywright;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-@UsePlaywright
+import java.util.Arrays;
+
+@UsePlaywright(AnAnnotatedPlaywrightTest.MyOptions.class)
 public class AnAnnotatedPlaywrightTest {
+
+    public static class MyOptions implements OptionsFactory {
+
+        @Override
+        public Options getOptions() {
+            return new Options()
+                    .setHeadless(false)
+                    .setLaunchOptions(
+                            new BrowserType.LaunchOptions()
+                                    .setArgs(Arrays.asList("--no-sandbox", "--disable-gpu"))
+                    );
+
+        }
+    }
 
     @Test
     void shouldShowThePageTitle(Page page) {
