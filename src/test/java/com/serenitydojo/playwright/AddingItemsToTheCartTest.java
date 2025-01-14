325e7dd37ffd38f2c9eedb0f05b3c2eb36084367
flaviaadiniz <flaviaadiniz@gmail.com>
2025-01-14 15:47:01 -0300

    Add class to perform tests adding items to the cart

---
+++ src/test/java/com/serenitydojo/playwright/AddingItemsToTheCartTest.java
@@ -0,0 +1,13 @@
+package com.serenitydojo.playwright;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserContext;
+import com.microsoft.playwright.Playwright;
+
+public class AddingItemsToTheCartTest {
+
+    protected static Playwright playwright;
+    protected static Browser browser;
+    protected static BrowserContext browserContext;
+
+}
